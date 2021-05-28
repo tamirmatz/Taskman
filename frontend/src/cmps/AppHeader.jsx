486c9440ca4b9f8f3945d34749e7678eae4efa72
harel hazan <hazandev@gmail.com>
2021-05-28 15:36:33 +0300

    header

--- frontend/src/cmps/AppHeader.jsx
+++ frontend/src/cmps/AppHeader.jsx
@@ -5,10 +5,10 @@ import { connect } from 'react-redux'
 class _AppHeader extends Component {
     render() {
         // const { loggedInUser } = this.props;
-        return <header className="main-header flex space-between">
-            <div className="logo fs28"><Link to={`/`}>Taskman</Link></div>
-            <nav className="main-nav">
-                <NavLink to="/">Home</NavLink>
+        return <header className="main-header flex space-between c-white fam-1 ps-3">
+            <div className="logo fs28 c-white fam-3"><Link to={`/`}>Taskman</Link></div>
+            <nav className="main-nav ">
+                    <NavLink to="/">Home</NavLink>
                     <NavLink to="/board">Boards</NavLink> 
                     <NavLink to="/">Login</NavLink> 
                     <NavLink to="/dashboard">Dashboard</NavLink>
