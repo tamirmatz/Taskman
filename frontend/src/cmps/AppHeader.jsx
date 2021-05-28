7fe4a8dd719afee6f2b5dc0c0eaadfabf63d2a59
tamirmatz <tamirmatz@gmail.com>
2021-05-28 15:21:30 +0300

    dfdf

--- frontend/src/cmps/AppHeader.jsx
+++ frontend/src/cmps/AppHeader.jsx
@@ -4,16 +4,21 @@ import { connect } from 'react-redux'
 
 class _AppHeader extends Component {
     render() {
-        const { loggedInUser } = this.props;
-        return <header className="main-header">
-
+        // const { loggedInUser } = this.props;
+        return <header className="main-header flex space-between">
+            <div className="logo fs28"><Link to={`/`}>Taskman</Link></div>
+            <nav className="main-nav">
+                <NavLink to="/">Home</NavLink>
+                    <NavLink to="/board">Boards</NavLink> 
+                    <NavLink to="/">Login</NavLink> 
+                    <NavLink to="/dashboard">Dashboard</NavLink>
+            </nav>
         </header>
     }
 
 }
 const mapStateToProps = state => {
     return {
-        loggedInUser: state.userModule.loggedInUser
     }
 }
 const mapDispatchToProps = {}
