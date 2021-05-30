a1aa09138702ed08de44961f0466dc971b026c5e
harel hazan <hazandev@gmail.com>
2021-05-30 16:19:52 +0300

    miniBoard

--- frontend/src/cmps/board/MiniBoard.jsx
+++ frontend/src/cmps/board/MiniBoard.jsx
@@ -2,7 +2,7 @@
 export function MiniBoard({title}) {
     return (
             <section className="miniBoard flex center content-center">
-                <h1 className="fam-1 font-1">{title}</h1>
+                <h1 className="title-miniBoard main-color fam-1 font-1">{title}</h1>
             </section>
     )
 }
