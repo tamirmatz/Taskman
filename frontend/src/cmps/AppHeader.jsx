584f5cc451aa3677b3581e6a60f33e8cd122a38d
harel hazan <hazandev@gmail.com>
2021-05-30 15:30:29 +0300

    logo

--- frontend/src/cmps/AppHeader.jsx
+++ frontend/src/cmps/AppHeader.jsx
@@ -6,7 +6,7 @@ class _AppHeader extends Component {
     render() {
         // const { loggedInUser } = this.props;
         return <header className="app-header flex space-between center fam-1  font-s c-white">
-            <div className="logo font-5 fam-3 ps-2"><Link to={`/`}>Taskman</Link></div>
+            <div className="logo font-5 ps-2"><Link to={`/`}><span className="logo-span">T</span>askman</Link></div>
             <nav className="main-nav w-20 flex space-evenly c-danger">
                     <NavLink to="/board">Boards</NavLink> 
                     <NavLink to="/">Login</NavLink> 
