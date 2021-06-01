e22007b15566f5b7bce82348c9e0403ea87e7953
Nadav <nadav1410@gmail.com>
2021-06-01 21:32:22 +0300

    dfsd

--- frontend/src/cmps/board/TaskPreview.jsx
+++ frontend/src/cmps/board/TaskPreview.jsx
@@ -96,7 +96,7 @@ export class TaskPreview extends Component {
 
 
                                     <h1 className="task-title fam-1 font-m">{task.title}</h1>
-                                    <div className="flex w-100 content-start  gap-xs font-s fam-1 c-stand">
+                                    <div className="task-mini-details flex w-100 content-start  gap-xs fam-1 c-stand">
                                         {utilService.isFalse(task.comments) && <small className="flex center"><FaRegCommentDots /></small>}
                                         {utilService.isFalse(task.checklists) && <div className={`flex row center ${boardService.checklistPreview(task).isDone && "check-list-done-prev"}`}>
                                             <BsCheckBox />
