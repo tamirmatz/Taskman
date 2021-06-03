96d0c48493d5a9d2043d6fff948fd93e007de406
harel hazan <hazandev@gmail.com>
2021-06-03 16:19:12 +0300

    import { AiOutlineClose } from 'react-icons/ai'

--- frontend/src/cmps/board/taskDetails/action/actionModal/GroupModal.jsx
+++ frontend/src/cmps/board/taskDetails/action/actionModal/GroupModal.jsx
@@ -31,7 +31,7 @@ class _GroupModal extends Component {
             <ModalAction>
                 <div className="group-modal p-abs flex column pad-07 c-stand z-1 br-3">
                     <div className="header-modal font-1 fam-1 fw-2 flex center space-between gap-5 w-100 mb-1">
-                        <h1 className="fam-1 font-1 ">List: {group.title}</h1>
+                        <h1 className="fam-1 font-2 ">List: {group.title}</h1>
                         <span className="cur-pointer fam-1 font-m bold" onClick={() => { this.props.toggleModal(`${groupId}`) }}><AiOutlineClose /></span>
                     </div>
                     <div className="action-content">
