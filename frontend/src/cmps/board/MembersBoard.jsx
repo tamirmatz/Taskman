4124cfe48a4626e8eeda18680a44ac7193450f77
harel hazan <hazandev@gmail.com>
2021-06-04 17:48:13 +0300

    sytyle

--- frontend/src/cmps/board/MembersBoard.jsx
+++ frontend/src/cmps/board/MembersBoard.jsx
@@ -7,7 +7,7 @@ class _MembersBoard extends Component {
     render() {
         const members = this.props.board.members;
         return (
-            <section className="members-board flex space-between">
+            <section className="members-board flex space-between ">
                 {members.map(member =>
                     <NavLink key={member._id} to={`/user/${member._id}`}>
                         <UserPreview user={member} />
